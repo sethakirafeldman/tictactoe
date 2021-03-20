53f67b169d65ec4e18227ce4035456a34e35378a
Seth <namdlefhtes@gmail.com>
2021-03-19 21:45:39 -0400

    trying to make loop to create 9 divs in DOM

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ const gameBoard = [""];
 let playerX = [""];
 
 let playerO = [""];
+
+let generateBoard = function(){
+    for (i=0; i < 8; i++ ){
+       let gameDiv =  getElementById("gameBoard");
+       div = document.createElement("div");
+        document.gameDiv.appendChild(div);
+    }
+
+}
+
+let clicker = function(i) {
+    let selectedBox = document.getElementById(i);
+    selectedBox.innerHTML = "X";
+}
