727176a1ac842d825b78e0b5f89dec7aad6095c7
namdlefhtes <namdlefhtes@gmail.com>
2021-05-23 12:01:52 -0400

    p1 stores selection positions

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 selectedBox = "";
-
 let m = "";
+const playerOne = { 
+placement: placement = []
+};
+
+const playerTwo = { };
 
 const generateBoard = () => {
     for (i=0; i < 9; i++ ){
@@ -19,7 +23,7 @@ const generateBoard = () => {
     });
   
   }
-
+  // sets computer to opposite of player sel. 
   const setComputer = () => {
       playerTwo.type = "computer";
       if (m === "X") {
@@ -29,7 +33,7 @@ const generateBoard = () => {
         playerTwo.marker = "X" 
       };
   }
-
+  //hides marker selection options, sets selection.
   function setMarker(m) {
       document.querySelectorAll(".marker-button").className="hide-button";
       document.getElementById("promptMe").innerText = `You have chosen ${m}` //template string :) 
@@ -46,22 +50,14 @@ need something that can scan through the dom an determine the first player
 to reach 3 in a row of any of these. loop or filter through 0-8 divs to check if winner. */
 const winners = {
   key:[ 
-    [0,1,2],[3,4,5],[6,7,8],[0,3,6],[0,4,8],[1,4,7],[2,5,8], [2,4,6]
+    [0,1,2],[3,4,5],[6,7,8],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[2,4,6]
   ],
   compare: [ ["","",""]] 
  };
 
-const playerOne = {
 
-};
 
-const playerTwo = {
-    type: "",
-    marker: ""
-}
-
- let xPlacement = [];
-
+ /*
  const checkWinner = () => {
   for (let j=0; j<8; j++) { 
 
@@ -84,22 +80,25 @@ const playerTwo = {
   };
 
  };
-
+*/
 
   // need to store player info and marker info in an object somewhere so that it can be used when checking winners. 
   //perhaps a factory function in 
   // order create objects to check against the winner arrays.
 
- let clicker = function(i) {   
+ let clicker = (i) => {   
     if (m === "X" || m ==="O") { 
       let selectedBox = document.getElementById(i);   
       selectedBox.innerText = m;
-      checkWinner();
+      //playerOne.placement[plaArr] = selectedBox.id;
+      let p1PlacementArr = playerOne.placement;
+      p1PlacementArr.push(selectedBox.id);
+      checkWinner(m);
     }
     else {
       alert("please select a marker");
     };
  };
 
-
-
+const checkWinner = (m) => { 
+}
